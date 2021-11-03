2aeeff456d9e484e01b1e427b01bc31df9c71b24
nyac-1 <routusampath@yahoo.com>
2021-11-03 11:15:36 +0530

    graphql arch and jobs+companies integration

---
+++ server/resolvers.js
@@ -0,0 +1,15 @@
+const db = require('./db.js');
+
+const Query = {
+    jobs: () => (db.jobs.list())
+}
+
+const Job = {
+    company: (job) => (db.companies.get(job.companyId))
+}
+
+
+module.exports = {
+    Query,
+    Job
+};
--- server/server.js
+++ server/server.js
@@ -4,6 +4,11 @@ const express = require('express');
 const expressJwt = require('express-jwt');
 const jwt = require('jsonwebtoken');
 const db = require('./db');
+const fs = require('fs');
+
+
+//GraphQL
+const {ApolloServer, gql} = require('apollo-server-express');
 
 const port = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
@@ -13,6 +18,11 @@ app.use(cors(), bodyParser.json(), expressJwt({
   secret: jwtSecret,
   credentialsRequired: false
 }));
+
+const typeDefs = gql(fs.readFileSync('./schema.graphql', {encoding: 'utf8'}));
+const resolvers = require('./resolvers.js');
+const apolloServer = new ApolloServer({typeDefs, resolvers});
+apolloServer.applyMiddleware({app, path: "/testing-gql"});
 
 app.post('/login', (req, res) => {
   const {email, password} = req.body;
